2469ab2142b663735ac97b2383425733c85c5e2a
crios2020 <intercrios@hotmail.com>
2020-08-20 12:58:08 -0300

    Add files via upload

---
+++ Auto.java
@@ -0,0 +1,18 @@
+//declaración de clases
+public class Auto{
+	//atributos
+	String marca;
+	String modelo;
+	String color;
+	int velocidad;
+	
+	//métodos
+	void acelerar(){
+		velocidad=velocidad+10;
+	}
+	
+	void frenar(){
+		velocidad=velocidad-10;
+	}
+
+}// end class
---
+++ Programa.java
@@ -0,0 +1,28 @@
+public class Programa{
+	public static void main(String[] args){
+		//punto de entrada de nuestro proyecto
+		System.out.println("Hola Mundo!");
+		
+		System.out.println("-- auto1 --");
+		Auto auto1=new Auto();	//creo un objeto de la clase Auto
+		auto1.marca="Fiat";
+		auto1.modelo="Toro";
+		auto1.color="Rojo";
+		auto1.acelerar();		//10
+		auto1.acelerar();		//20
+		auto1.acelerar();		//30
+		auto1.frenar();			//20
+		System.out.println(auto1.marca+" "+auto1.modelo+" "
+			+auto1.color+" "+auto1.velocidad);
+		
+		System.out.println("-- auto2 --");
+		Auto auto2=new Auto();
+		auto2.marca="Ford";
+		auto2.modelo="Ka";
+		auto2.color="Negro";
+		auto2.acelerar();
+		System.out.println(auto2.marca+" "+auto2.modelo+" "
+			+auto2.color+" "+auto2.velocidad);
+
+	}
+}
